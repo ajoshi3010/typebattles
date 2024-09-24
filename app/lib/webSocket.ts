350c5e90d7a0efaa2b168efb7f1b5240ddc0ad11
KarthikSrishty <karthikeyasrishty@gmail.com>
2024-09-24 17:53:51 +0530

    Stage-1

--- app/lib/webSocket.ts
+++ app/lib/webSocket.ts
@@ -1,1 +1,2 @@
-export const ws = new WebSocket("ws://localhost:8080", "echo-protocol");
+// export const ws = new WebSocket("ws://localhost:8080", "echo-protocol");
+export const ws = new WebSocket('ws://13.60.171.184:8080');
