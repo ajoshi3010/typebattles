6f3419d62bb61d2136c6653493368f6b8a1cdc84
KarthikSrishty <karthikeyasrishty@gmail.com>
2024-09-24 19:44:48 +0530

    ws-Update

--- app/lib/webSocket.ts
+++ app/lib/webSocket.ts
@@ -1,2 +1,5 @@
 // export const ws = new WebSocket("ws://localhost:8080", "echo-protocol");
-export const ws = new WebSocket('ws://13.60.171.184:8080');
+export const ws = new WebSocket('ws://13.51.150.172:8080');
+ws.onopen=()=>{
+    console.log("CONNECTED TO WS!");
+}
